79c5a2785f4c16bf91be4d8e42ade030d007eccf
Deovolente Mtavangu <d2.mtavangu@gmail.com>
2020-09-09 02:10:59 -0700

    final

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>CRUD</title>
+  </head>
+  <body>
+   <div class="container">
+   <div class="row">
+   <div class="col-md-12 mt-5">
+    <center><img src="252035.png" width="90px" height="100px"></center>
+    
+   <h1 class="text-center">Please Enter your Details</h1> 
+ </div>
+</div>
+<div class="row">
+  <div class="col-md-5 mx-auto">
+    <?php
+    include 'model.php';
+    $model = new Model();
+    $insert = $model->insert();
+    ?>
+    <form action="" method="post">
+      <div class="form-group">
+        <label for="">Name</label>
+        <input type="text" name="name" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="">Username</label>
+        <input type="text" name="username" class="form-control">   
+          </div>
+      <div class="form-group">
+        <label for="">Email</label>
+        <input type="email" name="email" class="form-control">
+      </div> 
+      <div class="form-group">
+        <label for="">Password</label>
+        <input type="text" name="password" class="form-control">
+      </div>
+       <div class="form-group">
+        <center>
+          <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </center>
+        
+      </div>
+    </form>
+  </div>
+</div>
+</div>
+<center><a href="records.php">VIEW RECORDS</a></center>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ model.php
@@ -0,0 +1,102 @@
+<?php 
+
+class Model{
+	private $server = "localhost";
+	private $username = "root";
+	private $password;
+	private $db ="oopcrud";
+	private $conn;
+	public function __construct(){
+		try{
+			$this->conn = new mysqli($this->server,$this->username,$this->password,$this->db);
+		}
+		catch(Exception $e){
+			echo "connection failed ". $e->getMessage();
+		}
+	}
+	public function insert(){
+		if (isset($_POST['submit'])){
+           if (isset($_POST['name']) && isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+           	if (!empty($_POST['name']) &&!empty($_POST['username']) && !empty($_POST['email']) &&!empty($_POST['password'])){
+           		$name = $_POST['name'];
+           		$username = $_POST['username'];
+           		$email = $_POST['email'];
+           		$password = $_POST['password'];
+
+           		$query = "INSERT INTO records(name,username,email,password) VALUES ('$name','$username','$email','$password')";
+           		if ($sql = $this->conn->query($query)){
+           			echo "<script>alert('record added successfully');</script>";
+           			echo "<script>window.location.href = 'index.php';</script>";
+           		}
+           		else 
+           		{ 
+           		echo "<script>alert('failed!');</script>";
+           		echo "<script>window.location.href = 'index.php';</script>";	
+           		}
+           	 }else
+           	   {
+                echo "<script>alert('empty!');</script>";
+           		echo "<script>window.location.href = 'index.php';</script>";	
+           		}
+
+           	}
+           }
+		}
+	public function fetch(){
+		$data = null;
+		$query = "SELECT * FROM records";
+		if($sql = $this->conn->query($query)){
+			while ($row = mysqli_fetch_assoc($sql)){
+			   $data[] = $row;
+			}
+		}
+		return $data;
+	}
+	public function delete($id){
+		$query = "DELETE FROM records where id = '$id'";
+		if ($sql = $this->conn->query($query)){
+			return true;
+		} else {
+			return false;
+		}
+	} 
+	public function fetch_single($id){
+
+			$data = null;
+
+			$query = "SELECT * FROM records WHERE id = '$id'";
+			if ($sql = $this->conn->query($query)) {
+				while ($row = $sql->fetch_assoc()) {
+					$data = $row;
+				}
+			}
+			return $data;
+		}
+		
+		public function edit($id){
+
+			$data = null;
+
+			$query = "SELECT * FROM records WHERE id = '$id'";
+			if ($sql = $this->conn->query($query)) {
+				while($row = $sql->fetch_assoc()){
+					$data = $row;
+				}
+			}
+			return $data;
+		}
+		public function update($data){
+
+			$query = "UPDATE records SET name='$data[name]', username='$data[username]', email='$data[email]', password='$data[password]' WHERE id='$data[id] '";
+
+			if ($sql = $this->conn->query($query)) {
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+	}
+
+
+?>
